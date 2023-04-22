21165cd4557ce76b4f0333ef382cc24ba32bb2ea
Sadeghdrr <sadegh.sg.sn@gmail.com>
2023-04-22 21:47:39 +0430

    People classes compressed and finished

--- src/main/java/model/people/Engineer.java
+++ src/main/java/model/people/Engineer.java
@@ -1,9 +1,22 @@
 package model.people;
 
+import model.Governance;
+import model.Stronghold;
+import model.User;
+import model.people.enums.Hp;
+import model.people.enums.Speed;
+
 public class Engineer extends Units {
     private boolean inWork = false;
     private boolean hasOilPail = false;
     private boolean emptyPail = true;
+
+    public Engineer() {
+        this.hp = Hp.VERY_LOW.getHp();
+        this.cost = 30;
+        this.speed = Speed.HIGH;
+        this.ownerGovernance = Stronghold.getCurrentGovernance();
+    }
 
     public boolean isInWork() {
         return inWork;
--- src/main/java/model/people/Troops.java
+++ src/main/java/model/people/Troops.java
@@ -1,14 +1,46 @@
 package model.people;
 
+import model.Stronghold;
+import model.people.enums.TroopTypes;
 import model.resources.TroopEquipment;
 
-public abstract class Troops extends Units {
+public class Troops extends Units {
+    protected final TroopTypes type;
     protected TroopEquipment weaponType;
     protected TroopEquipment armorType;
     protected int damage;
     protected boolean hasHorse;
     protected boolean isArab;
     protected boolean hasFiringWeapon;
+    protected final boolean canScaleWall;
+    protected final boolean canDigKhandagh;
+    protected final int range;
+    protected final double damageRatioOnArmor;
+    protected boolean revealed;
+
+
+    public Troops(TroopTypes TROOP) {
+        this.type = TROOP;
+        this.hp = TROOP.getHp();
+        this.speed = TROOP.getSpeed();
+        this.weaponType = TROOP.getWeaponType();
+        this.armorType = TROOP.getArmorType();
+        this.damage = TROOP.getDamage();
+        this.hasHorse = TROOP.hasHorse();
+        this.isArab = TROOP.isArab();
+        this.hasFiringWeapon = TROOP.hasFiringWeapon();
+        this.range = TROOP.getRange();
+        this.damageRatioOnArmor = TROOP.getDamageRatioOnArmor();
+        this.canScaleWall = TROOP.canScaleWall();
+        this.canDigKhandagh = TROOP.canDigKhandagh();
+        this.revealed = TROOP.isRevealed();
+        this.cost = TROOP.getCost();
+        this.ownerGovernance = Stronghold.getCurrentGovernance();
+    }
+
+    public TroopTypes getType() {
+        return type;
+    }
 
     public TroopEquipment getWeaponType() {
         return weaponType;
@@ -32,5 +64,29 @@ public abstract class Troops extends Units {
 
     public boolean hasFiringWeapon() {
         return hasFiringWeapon;
+    }
+
+    public boolean canScaleWall() {
+        return canScaleWall;
+    }
+
+    public boolean canDigKhandagh() {
+        return canDigKhandagh;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public double getDamageRatioOnArmor() {
+        return damageRatioOnArmor;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public void setRevealed(boolean revealed) {
+        this.revealed = revealed;
     }
 }
