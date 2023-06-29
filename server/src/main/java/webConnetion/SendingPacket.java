22f0e0d6f1b867a4164dca3747b0ca6d3cb4d022
Sadegh Sargeran <sadegh.sg.sn@gmail.com>
2023-06-29 13:09:51 +0430

    add server pocket receiving and sending

---
+++ server/src/main/java/webConnetion/SendingPacket.java
@@ -0,0 +1,15 @@
+package webConnetion;
+
+import java.util.ArrayList;
+
+public class SendingPacket {
+    private Object value;
+
+    public SendingPacket(Object value) {
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
